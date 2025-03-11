dfc7fe787978a140410e5532a4f4ba97d0fa3fdd
Leopoldo Peixoto Cardoso <peixoto.leopoldo@outlook.com>
2025-03-10 22:41:04 -0300

    atualiza codigo terraform

--- provider.tf
+++ provider.tf
@@ -1,13 +1,3 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "5.90.1"
-#     }
-#   }
-# }
-
-
 terraform {
   required_providers {
     aws = {
